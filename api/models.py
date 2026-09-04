682759ff1322844375baea4c9c46743ae44f6e9d
lindomarbatistao <lindomarbatistao@gmail.com>
2026-09-04 16:55:04 -0300

    Entramos no /admin e criamos api/urls.py vazio

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=255)
+    descricao = models.TextField()
     ativa = models.BooleanField(default=True)
 
     def __str__(self):
